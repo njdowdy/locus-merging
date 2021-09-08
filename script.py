8ee4fe97db34979540c6743a80766a95d7a86d65
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-09-08 11:50:41 -0500

    New template files added.

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from functions import import_funcs as imps
 
 if __name__ == '__main__':
-    imps.import_data('/input/input.csv')
+    df = imps.import_data('./input/combined.csv')
+    # print(df)
